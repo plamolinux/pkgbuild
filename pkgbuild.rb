e29fe4b3fac214f4ff0df58c2c2b809685358fde
KATOH Yasufumi <karma@jazz.email.ne.jp>
2024-02-16 22:41:21 +0900

    LXC -> Incus 利用に切り替え中

--- pkgbuild.rb
+++ pkgbuild.rb
@@ -101,23 +101,21 @@ class PkgBuild
     @mirror_path = "/pub/linux/Plamo"
     @release = release
     @majorver = @release[0].to_i
-    if @majorver == 6 then
-      @addon_pkgs = "plamo/05_ext/devel2.txz/git plamo/02_x11/expat #{addon}"
-      @@category = ["02_x11", "03_xclassics", "04_xapps",
-                    "05_ext", "06_xfce", "07_kde", "11_mate", "08_tex"]
-    elsif @majorver >= 7 then
-      # These addon pkgs needs from docbook2man
-      @addon_pkgs = "#{addon} plamo/05_ext/perl_Parse_Yapp plamo/05_ext/perl_XML_NamespaceSupport plamo/03_libs/libxslt \
-            plamo/05_ext/perl_SGMLSpm plamo/05_ext/perl_XML_SAX plamo/05_ext/perl_URI plamo/05_ext/perl_XML_SAX_Base plamo/05_ext/bind_tools plamo/03_libs/json_c plamo/03_libs/nss"
-      @@category = ["03_libs", "04_x11", "05_ext", "06_xapps", "07_multimedia", "08_daemons",
-                    "10_xfce", "11_lxqt", "12_mate", "13_tex" "16_virtualization"]
-    end
-    p @addon_pkgs
-    @ignore_pkgs = "firefox thunderbird kernel "
-  end
+    @ct_category = Array.new
+    @addon_pkgs = Array.new
 
-  def get_lxc_version
-    return IO.popen("lxc-ls --version").getc.to_i
+    # These addon pkgs needs from docbook2man
+    @addon_pkgs = ["plamo/05_ext/perl_Parse_Yapp", "plamo/05_ext/perl_XML_NamespaceSupport", "plamo/03_libs/libxslt",
+                   "plamo/05_ext/perl_SGMLSpm", "plamo/05_ext/perl_XML_SAX", "plamo/05_ext/perl_URI", "plamo/05_ext/perl_XML_SAX_Base",
+                   "plamo/05_ext/bind_tools", "plamo/03_libs/json_c", "plamo/03_libs/nss"]
+    if ! addon.empty? then
+      @addon_pkgs << addon
+    end
+    @@category = ["03_libs", "04_x11", "05_ext", "06_xapps", "07_multimedia", "08_daemons",
+                    "10_xfce", "11_lxqt", "12_mate", "13_tex" "16_virtualization"]
+
+    p @addon_pkgs
+    @ignore_pkgs = "firefox,thunderbird,kernel"
   end
 
   def get_package_info
@@ -127,140 +125,129 @@ class PkgBuild
   end
 
   def define_ct_category
-    if @majorver == 6 then
-      ct_category = "00_base 01_minimum 05_ext/docbook.txz "
-      if @package_category == "00_base" ||
-         @package_category == "01_minimum" ||
-         @package_category == "02_devel" then
-        @ct_category = ct_category
-        return
-      end
-      @@category.each{|c|
-        ct_category << "#{c} "
-        if c == @package_category
-          break
-        end
-      }
-    elsif @majorver >= 7 then
-      ct_category = "00_base 01_minimum 02_devel 09_printings "
-      if @package_name.index("grub") then
-        @addon_pkgs = "#{@addon_pkgs} plamo/04_x11/fonts.txz/dejavu_fonts_ttf plamo/05_ext/fuse2 plamo/03_libs/freetype"
-        ct_category << "03_libs "
-      elsif @package_name.index("vala") then
-        @addon_pkgs = "#{@addon_pkgs} plamo/03_libs/glib"
-      elsif @package_name.index("sqlite") then
-        @addon_pkgs = "#{@addon_pkgs} plamo/06_xapps/tcl"
-      elsif @package_name.index("gtk4") then
-        @addon_pkgs = "#{@addon_pkgs} plamo/07_multimedia/gstreamer.txz/gst_plugins_bad plamo/07_multimedia/gstreamer.txz/gstreamer plamo/07_multimedia/gstreamer.txz/orc plamo/07_multimedia/gstreamer.txz/gst_plugins_base"
-      end
+    ct_category = ["00_base", "01_minimum", "02_devel", "09_printings"]
+    if @package_name.index("grub") then
+      @addon_pkgs << ["plamo/04_x11/fonts.txz/dejavu_fonts_ttf", "plamo/05_ext/fuse2 plamo/03_libs/freetype"]
+      ct_category << "03_libs"
+    elsif @package_name.index("vala") then
+      @addon_pkgs << "plamo/03_libs/glib"
+    elsif @package_name.index("sqlite") then
+      @addon_pkgs << "plamo/06_xapps/tcl"
+    elsif @package_name.index("gtk4") then
+      @addon_pkgs << ["plamo/07_multimedia/gstreamer.txz/gst_plugins_bad", "plamo/07_multimedia/gstreamer.txz/gstreamer", "plamo/07_multimedia/gstreamer.txz/orc", "plamo/07_multimedia/gstreamer.txz/gst_plugins_base"]
+
       case @package_category
       when "03_libs" then
-        ct_category << "03_libs "
+        ct_category << "03_libs"
       when "04_x11" then
-        ct_category << "03_libs 04_x11 "
+        ct_category << ["03_libs", "04_x11"]
       when "05_ext" then
-        ct_category << "03_libs 04_x11 05_ext "
+        ct_category << ["03_libs", "04_x11", "05_ext"]
       when "06_xapps" then
-        ct_category << "03_libs 04_x11 05_ext 06_xapps "
+        ct_category << ["03_libs", "04_x11", "05_ext", "06_xapps"]
       when "07_multimedia" then
-        ct_category << "03_libs 04_x11 05_ext 06_xapps 07_multimedia "
+        ct_category << ["03_libs", "04_x11", "05_ext", "06_xapps", "07_multimedia"]
       when "08_daemons" then
-        ct_category << "03_libs 05_ext 08_daemons "
-        @ignore_pkgs << "gpicview keybinder libfm libfm_extra lxappearance \
-                lxappearance_obconf lxde_common lxde_icon_theme lxinput \
-                lxmenu_data lxpanel lxrandr lxsession lxtask lxterminal \
-                menu_cache pcmanfm"
+        ct_category << ["03_libs", "05_ext", "08_daemons"]
+        @ignore_pkgs << ",gpicview,keybinder,libfm,libfm_extra,lxappearance,"\
+                         "lxappearance_obconf,lxde_common,lxde_icon_theme,lxinput,"\
+                         "lxmenu_data,lxpanel,lxrandr,lxsession,lxtask,lxterminal,"\
+                         "menu_cache,pcmanfm"
       when "09_printings" then
-        ct_category << "03_libs 04_x11"
+        ct_category << ["03_libs", "04_x11", "05_ext"]
       when "10_xfce" then
-        ct_category << "03_libs 04_x11 05_ext 06_xapps 07_multimedia 10_xfce "
+        ct_category << ["03_libs", "04_x11", "05_ext", "06_xapps", "07_multimedia", "10_xfce"]
       when "11_lxqt" then
-        ct_category << "03_libs 04_x11 05_ext 06_xapps 07_multimedia 11_lxqt "
+        ct_category << ["03_libs", "04_x11", "05_ext", "06_xapps", "07_multimedia", "11_lxqt"]
       when "12_mate" then
-        ct_category << "03_libs 04_x11 05_ext 06_xapps 07_multimedia 12_mate "
+        ct_category << ["03_libs", "04_x11", "05_ext", "06_xapps", "07_multimedia", "12_mate"]
       when "13_tex" then
-        ct_category << "03_libs 04_x11 "
+        ct_category << ["03_libs", "04_x11"]
       when "16_virtualization" then
-        ct_category << "03_libs "
-        @addon_pkgs = "#{@addon_pkgs} plamo/05_ext/fuse3"
+        ct_category << "03_libs"
+        @addon_pkgs << "plamo/05_ext/fuse3"
       end
     end
-    output_log("Installed packages to container are \"#{ct_category}\"")
-    output_log("Addon packages are \"#{@addon_pkgs}\"")
+    cat_str = ct_category.to_s
+    addon_str = @addon_pkgs.to_s
+    output_log("Installed packages to container are \"#{cat_str}\"")
+    output_log("Addon packages are \"#{addon_str}\"")
     @ct_category = ct_category
   end
 
-  def define_create_ct_env
-    env = ""
-    env << %!CATEGORIES="#{@ct_category}" !
-    env << %!MIRRORSRV="#{@mirror_srv}" !
-    env << %!MIRRORPATH="#{@mirror_path}" !
-    env << %!ADDONPKGS="#{@addon_pkgs}" !
-    env << %!IGNOREPKGS="#{@ignore_pkgs}" !
+  def customize_ct_config(arch)
+    command = "incus config device add pkgbuild root_gnupg disk source=/root/.gnupg path=/root/.gnupg shift=yes"
+    system(command)
   end
 
-  def customize_ct_config(arch)
-    path = "/var/lib/lxc/pkgbuild_#{arch}/config"
-    FileUtils.cp(path, path + ".orig")
-    config = File.open(path, "a+")
-    config.puts("lxc.mount.entry = #{ENV['HOME']}/.gnupg root/.gnupg none bind 0 0")
-    config.close
+  def install_category_package(arch)
+    define_ct_category
+    url = "https://#{@mirror_srv}/#{@mirror_path}/Plamo-#{release}/#{arch}/"
+    @ct_category.each {|cat|
+      command =
+        "wget -nv -e robots=off -r -l 2 -nd -A .tgz,.txz,.tzst "\
+        "-R #{@ignore_pkgs}"\
+        "-X #{url}/plamo/#{cat}/old"\
+        "#{url}/plamo/#{cat}/"
+      if ! system("incus exec pkgbuild -- #{command}")
+        output_err("package download is failed")
+        return false
+      end
+    }
+    @addon_pkgs.each {|pkg|
+      command =
+        %|wget -nv -e robots=off -r -l 2 -nd -A "$(basename #{pkg})"-* -X "#{url}"/"$(dirname #{pkg})"/old #{url}/$(dirname #{pkg})|
+      if ! system("incus exec pkgbuild -- #{command}") then
+        output_err("addon packages download is failed")
+        return false
+      end
+    }
+    return true
   end
 
   def create_ct(arch, opt={})
     get_package_info
-    define_ct_category
-    env = define_create_ct_env
-    if opt.length > 0
-      option = " "
-      opt.each{|key,val|
-        option << key << " " << val
-      }
-    end
-    command = "#{env} lxc-create -n pkgbuild_#{arch} #{option} -t plamo -l #{@ct_loglevel} -- -a #{arch} -r #{@release} -c"
+    command = "incus launch images:plamo/#{@release} pkgbuild"
     system(command)
     customize_ct_config(arch)
-    Dir.mkdir("/var/lib/lxc/pkgbuild_#{arch}/rootfs/root/.gnupg")
-    if @majorver >= 7 then
-      FileUtils.touch("/var/lib/lxc/pkgbuild_#{arch}/rootfs/etc/resolv.conf", :verbose => true)
-    end
+    install_category_package(arch)
   end
 
   def ct_exist?(arch)
-    command = "lxc-info -n pkgbuild_#{arch} > /dev/null 2>&1"
+    command = "incus info pkgbuild > /dev/null 2>&1"
     output_log("execute #{command}")
     system(command)
   end
 
   def start_ct(arch)
-    command = "lxc-start -n pkgbuild_#{arch} -d -l #{@ct_loglevel}"
+    command = "incus start pkgbuild > /dev/null 2>&1"
     output_log("execute \"#{command}\"")
     system(command)
   end
 
   def ct_running?(arch)
-    command = "lxc-info -s -n pkgbuild_#{arch} | grep RUNNING"
+    command = "incus info | grep RUNNING"
     system(command)
   end
 
   def destroy_ct(arch)
-    command = "lxc-stop -n pkgbuild_#{arch} -l #{@ct_loglevel}"
+    command = "incus stop pkgbuild"
     if ! system(command) then
-      output_err("Failed to stop container pkgbuild_#{arch}")
+      output_err("Failed to stop container pkgbuild")
       exit 1
     end
-    output_log("container pkgbuild_#{arch} has been stopped.")
-    command = "lxc-destroy -n pkgbuild_#{arch} -l #{@ct_loglevel}"
+    output_log("container pkgbuild has been stopped.")
+    command = "incus delete pkgbuild"
     if system(command) then
-      output_log("container pkgbuild_#{arch} has been destroyed.")
+      output_log("container pkgbuild has been destroyed.")
     else
-      output_err("Failed to destroy container pkgbuild_#{arch}")
+      output_err("Failed to destroy container pkgbuild")
       exit 1
     end
   end
 
   def check_network(arch)
-    command = %(lxc-attach -n pkgbuild_#{arch} -- /bin/bash -c "dig github.com")
+    command = %(incus exec pkgbuild -- /bin/bash -c "dig github.com")
     cnt = 0
     output_log("Check network is alive")
     while ! system(command) do
@@ -280,14 +267,12 @@ class PkgBuild
     repo = PlamoSrc.new
     repo.compare_branch = branch
     if !ct_running?(arch) then
-      output_log("container is not running. start container pkgbuild_#{arch}.")
+      output_log("container is not running. start container pkgbuild.")
       start_ct(arch)
     end
 
     # For debug
-    common = %(lxc-attach -n pkgbuild_#{arch} -- /bin/bash -c )
-    # common = %(lxc-attach -v GIT_CURL_VERBOSE=1 -n pkgbuild_#{arch} -- /bin/bash -c )
-
+    common = %(incus exec pkgbuild -- /bin/bash -c )
 
     # remove all libtool archive files in the container
     command = %(#{common} "/usr/bin/remove-la-files.sh")
@@ -297,11 +282,12 @@ class PkgBuild
     end
 
     # clone Plamo-src if not exists
-    if !Dir.exist?("/var/lib/lxc/pkgbuild_#{arch}/rootfs/Plamo-src") then
+    command = %(#{common} "stat /Plamo-src > /dev/null 2>&1")
+    if ! system(command) then
       output_log("Waiting for starting container")
       check_network(arch)
 
-      command = %(#{common} "git clone #{repo.remote_repo}")
+      command = %(#{common} "cd / && git clone #{repo.remote_repo}")
       output_log("execute \"#{command}\"")
       if ! system(command) then
         output_err("git clone failed: #{$?}")
@@ -366,15 +352,12 @@ class PkgBuild
     if @majorver == 6 then
       levelstr = "P"
     end
-    Dir.glob("/var/lib/lxc/pkgbuild_#{arch}/rootfs/Plamo-src/#{@package_path}/*-#{levelstr}*.t*").each {|fullpath|
-      pkgfile = File.basename(fullpath)
-      begin
-        FileUtils.copy(fullpath, "./#{pkgfile}")
-      rescue
-        return false
-      end
-    }
-    return true
+    command = "incus file pull pkgbuild/Plamo-src/#{@package_path}/*.t*"
+    if system(command) then
+      return true
+    else
+      return false
+    end
   end
 
   def install_package(arch)
@@ -383,7 +366,7 @@ class PkgBuild
       levelstr = "P"
     end
     path_in_container = "/Plamo-src/#{@package_path}/*-#{levelstr}*.t*"
-    command = %(lxc-attach -n pkgbuild_#{arch} -- /bin/bash -c "updatepkg -f #{path_in_container}")
+    command = %(incus exec pkgbuild -- /bin/bash -c "updatepkg -f #{path_in_container}")
     output_log("exec command: #{command}")
     if ! system(command) then
       output_err("#{command} failed")
